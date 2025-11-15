421e06a15ea0398fff5d9231a208c3813cc64e2d
shambu2 <mugalishambu601@gmail.com>
2025-11-16 02:48:14 +0530

    finished Product section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HeroComponent } from "../../components/hero/hero.component";
+import { ProductsComponent } from "../../components/products/products.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, ProductsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
