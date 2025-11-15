b49a58f6d4137892a661a236c099d6ae30f77fcc
shambu2 <mugalishambu601@gmail.com>
2025-11-15 22:19:57 +0530

    added hero section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { HeroComponent } from "../../components/hero/hero.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
