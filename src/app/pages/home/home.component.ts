3b34d28ba51fd7fc7d8c1d5458db875b532717fe
shambu2 <mugalishambu601@gmail.com>
2025-11-16 04:45:05 +0530

    finished working section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HeroComponent } from "../../components/hero/hero.component";
 import { ProductsComponent } from "../../components/products/products.component";
+import { WorkingComponent } from "../../components/working/working.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent, ProductsComponent],
+  imports: [NavbarComponent, HeroComponent, ProductsComponent, WorkingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
