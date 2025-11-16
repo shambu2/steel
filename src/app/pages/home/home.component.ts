e209ca6aafbf33eda1992e32aebff477b67490b8
shambu2 <mugalishambu601@gmail.com>
2025-11-16 19:27:15 +0530

    feature section completed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HeroComponent } from "../../components/hero/hero.component";
 import { ProductsComponent } from "../../components/products/products.component";
 import { WorkingComponent } from "../../components/working/working.component";
+import { FeaturesComponent } from "../../components/features/features.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent, ProductsComponent, WorkingComponent],
+  imports: [NavbarComponent, HeroComponent, ProductsComponent, WorkingComponent, FeaturesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
